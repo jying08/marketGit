404320fa7a3a297f842b2f8d89404409b8cc0676
jhimyW <chance.jhimy@gmail.com>
2024-05-15 02:31:39 -0500

    primer commir, se le conecto a la database marketGit y le crea la estructura orientado al dominio

---
+++ src/main/java/com/market/git/MarketGitApplication.java
@@ -0,0 +1,13 @@
+package com.market.git;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketGitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MarketGitApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/market/git/MarketGitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.market.git;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketGitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
